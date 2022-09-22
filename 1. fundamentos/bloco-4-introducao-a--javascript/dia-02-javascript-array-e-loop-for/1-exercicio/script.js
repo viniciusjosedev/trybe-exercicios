e900f81e144e5920d432cb07f3ddb27a345b78f6
Vinicius José <viniciusjosedev@gmail.com>
2022-09-22 20:39:53 -0300

    testes de iteradores finalizados

--- 1. fundamentos/bloco-4-introducao-a--javascript/dia-02-javascript-array-e-loop-for/1-exercicio/script.js
+++ 1. fundamentos/bloco-4-introducao-a--javascript/dia-02-javascript-array-e-loop-for/1-exercicio/script.js
@@ -1,4 +1,4 @@
-// meus testes
+// meus testes de arrays
 
 // list = [1,2,3,4,5]
 // console.log(list)
@@ -23,3 +23,20 @@
 // let menu = ['Home', 'Serviços', 'Portfólio', 'Links'];
 // menu.push('Contato')
 // console.log(menu);
+
+// Meus testes de for
+
+// let groceryList = ['Arroz', 'Feijão', 'Alface', 'Melancia'];
+// for(i = 0; i < groceryList.length; i+=1){
+// 	console.log(groceryList[i])
+// }
+
+// let names = ['João', 'Maria', 'Antônio', 'Margarida']
+// for (let i of names) {
+// 	console.log(i)
+// }
+
+// Meus teste de while
+
+
+
