3b8c3de2c1ac25f0c21367bd3acebf93a70f59e5
Vinicius José <viniciusjosedev@gmail.com>
2022-09-22 09:54:12 -0300

    Exercicios básicos inicias de exemplos finalizados

--- 1. fundamentos/bloco-4-introducao-a--javascript/dia-01-javascript-primeiros-passos/1-exercicio/script.js
+++ 1. fundamentos/bloco-4-introducao-a--javascript/dia-01-javascript-primeiros-passos/1-exercicio/script.js
@@ -7,14 +7,67 @@
 
 // Exercicio 3
 
-const nota = 40;
+// const nota = 40;
 
-if (nota >= 80){
-	console.log('Parabéns, você foi aprovada(o)!')
-}
-else if( nota < 80 && nota>=60){
-	console.log('Você está na nossa lista de espera')
-}
-else{
-	console.log('Você foi reprovada(o)')
-}
+// if (nota >= 80){
+// 	console.log('Parabéns, você foi aprovada(o)!')
+// }
+// else if( nota < 80 && nota>=60){
+// 	console.log('Você está na nossa lista de espera')
+// }
+// else{
+// 	console.log('Você foi reprovada(o)')
+// }
+
+// Exercicio 4
+
+// const currentHoour = 22;
+// message = "";
+// if (currentHoour>=4 && currentHoour<11){
+// 	message = 'Hmmm, cheiro de café recém-passado'
+// }
+// else if (currentHoour>=11 && currentHoour<14){
+// 	message = 'Hora do almoço!!!” na variável “message'
+// }
+// else if (currentHoour>=14 && currentHoour<18){
+// 	message = 'Vamos fazer um bolo pro café da tarde?'
+// }
+// else if (currentHoour>=18 && currentHoour<22){
+// 	message = 'Rango da noite, vamos jantar :D'
+// }
+// else if (currentHoour>=22){
+// 	message = 'Não deveríamos comer nada, é hora de dormir'
+// }
+
+// console.log(message);
+
+// Exercicio 5
+
+// weekDay = 5;
+
+// if(weekDay =='segunda-feira' || weekDay == 'terça-feira' || weekDay == 'quarta-feira' || 
+// weekDay == weekDay == 'quinta-feira' || weekDay == 'sexta-feira'){
+// 	console.log('Oba, mais um dia de aprendizado na Trybe >:D')
+// }
+// else{
+// 	console.log('FINALMENTE, descanso merecido UwU')
+// }
+
+// Exercicio 6
+
+// resultado = 'a'
+
+// switch (resultado){
+// 	case 'aprovada':
+// 		console.log('Parabéns, você foi aprovada(o)!');
+// 		break;
+// 	case 'lista':
+// 		console.log('Você está na nossa lista de espera');
+// 		break;
+// 	case 'reprovada':
+// 		console.log("Você foi reprovada(o)");
+// 		break;
+// 	default:
+// 		console.log("Informação incorreta")
+// 		break;
+// }
