b199889a0ae95fa2152265f84f27fb901028a428
Vinicius José <viniciusjosedev@gmail.com>
2022-09-21 21:10:06 -0300

    segundo exercicio finalizado

--- 1. fundamentos/bloco-4-introducao-a--javascript/dia-01-javascript-primeiros-passos/1-exercicio/script.js
+++ 1. fundamentos/bloco-4-introducao-a--javascript/dia-01-javascript-primeiros-passos/1-exercicio/script.js
@@ -1,5 +1,20 @@
-const base = 5;
-const heigth = 8;
-const area = base*heigth
-const perimeter = base+heigth+area
-console.log(base, heigth, area, perimeter)
+// Exercicio 2:
+// const base = 5;
+// const heigth = 8;
+// const area = base*heigth
+// const perimeter = base+heigth+area
+// console.log(base, heigth, area, perimeter)
+
+// Exercicio 3
+
+const nota = 40;
+
+if (nota >= 80){
+	console.log('Parabéns, você foi aprovada(o)!')
+}
+else if( nota < 80 && nota>=60){
+	console.log('Você está na nossa lista de espera')
+}
+else{
+	console.log('Você foi reprovada(o)')
+}
