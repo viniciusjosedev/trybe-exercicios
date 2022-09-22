ec7b7243984227cdc9be38eb983eee693eb8b730
Vinicius José <viniciusjosedev@gmail.com>
2022-09-22 15:35:12 -0300

    aula de array (listas) concluidas, testes feitos

---
+++ 1. fundamentos/bloco-4-introducao-a--javascript/dia-02-javascript-array-e-loop-for/1-exercicio/script.js
@@ -0,0 +1,25 @@
+// meus testes
+
+// list = [1,2,3,4,5]
+// console.log(list)
+// list.push('oi')
+// console.log(list)
+// list.unshift('um')
+// console.log(list)
+// list.shift()
+// console.log(list)
+// console.log(list.indexOf('oi'))
+
+// let menu = ['Home', 'Serviços', 'Portfólio', 'Links'];
+// let menuServices = menu[1];
+
+// console.log([menuServices]);
+
+// let menu = ['Home', 'Serviços', 'Portfólio', 'Links'];
+// let indexOfPortfolio = menu.indexOf('Portfólio');
+
+// console.log(indexOfPortfolio);
+
+// let menu = ['Home', 'Serviços', 'Portfólio', 'Links'];
+// menu.push('Contato')
+// console.log(menu);
