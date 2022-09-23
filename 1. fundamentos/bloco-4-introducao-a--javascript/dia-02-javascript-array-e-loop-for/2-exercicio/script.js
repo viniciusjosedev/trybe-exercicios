671b236ac772d83d067493e818a48655b174e0e7
Vinicius José <viniciusjosedev@gmail.com>
2022-09-22 21:26:44 -0300

    maioria dos requisitos feitos

--- 1. fundamentos/bloco-4-introducao-a--javascript/dia-02-javascript-array-e-loop-for/2-exercicio/script.js
+++ 1. fundamentos/bloco-4-introducao-a--javascript/dia-02-javascript-array-e-loop-for/2-exercicio/script.js
@@ -3,3 +3,61 @@ let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 // 	console.log(numbers[i])
 // }
 
+// soma = 0
+// for (i = 0; i < numbers.length; i+=1){
+// 	soma += numbers[i]
+// }
+// console.log(soma)
+
+// soma = 0
+// for (i = 0; i < numbers.length; i+=1){
+// 	soma += numbers[i]
+// }
+// console.log(soma/numbers.length)
+
+// soma = 0
+// for (i = 0; i < numbers.length; i+=1){
+// 	soma += numbers[i]
+// }
+// if (soma/numbers.length>=20){
+// 	console.log("Valor maior que 20")
+// }
+// else{
+// 	console.log("Valor menor que 20")
+// }
+
+// num = 0
+// for (i = 0; i < numbers.length; i+=1){
+// 	if (i===0){
+// 		num = numbers[i]
+// 	}
+// 	else if (numbers[i]>num){
+// 		num = numbers[i]
+// 	}
+// }
+// console.log(num)
+
+// cont = 0
+// for (i = 0; i < numbers.length; i+=1){
+// 	if (numbers[i]%2!==0){
+// 		cont += 1
+// 	}
+// }
+// console.log(cont)
+
+// num = 0
+// for (i = 0; i < numbers.length; i+=1){
+// 	if (i===0){
+// 		num = numbers[i]
+// 	}
+// 	else if (numbers[i]<num){
+// 		num = numbers[i]
+// 	}
+// }
+// console.log(num)
+
+lista = [1,]
+for (i=1; i==25; i+=1){
+	lista.push(i)
+}
+console.log(lista)
