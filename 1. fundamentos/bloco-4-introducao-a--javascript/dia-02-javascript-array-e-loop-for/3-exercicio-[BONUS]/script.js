0a09532204cc05b1fc8a6304938c75a510b89309
Vinicius José <viniciusjosedev@gmail.com>
2022-09-23 21:49:18 -0300

    exercicio principal finalizado

--- 1. fundamentos/bloco-4-introducao-a--javascript/dia-02-javascript-array-e-loop-for/3-exercicio-[BONUS]/script.js
+++ 1. fundamentos/bloco-4-introducao-a--javascript/dia-02-javascript-array-e-loop-for/3-exercicio-[BONUS]/script.js
@@ -23,13 +23,21 @@ let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 // }
 // console.log(numbers)
 
-novoNumbers = []
-for (i=0; i<numbers.length; i+=1){
-	if (i===numbers.length-1){
-		novoNumbers.push(numbers[i]*2)
-	}
-	else{
-		novoNumbers.push(numbers[i]*numbers[i+1])
-	}
+// novoNumbers = []
+// for (i=0; i<numbers.length; i+=1){
+// 	if (i===numbers.length-1){
+// 		novoNumbers.push(numbers[i]*2)
+// 	}
+// 	else{
+// 		novoNumbers.push(numbers[i]*numbers[i+1])
+// 	}
+// }
+// console.log(novoNumbers)
+
+
+
+soma = 0
+for (i=1; i<=50; i+=1){
+	soma+=i
 }
-console.log(novoNumbers)
+console.log(soma)
