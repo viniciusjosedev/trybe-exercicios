df9fef0a47658005530f1dc8deeecbe0ef3d6151
Vinicius José <viniciusjosedev@gmail.com>
2022-09-27 12:52:00 -0300

    exercicios de dicionrios(objetos) feits

---
+++ 1. fundamentos/bloco-4-introducao-a--javascript/dia-04-javascript-objetos-e-funcoes/01-exercicios/script.js
@@ -0,0 +1,108 @@
+// Exercicio 1
+
+// let info = {
+// 	personagem: 'Margarida',
+// 	origem: 'Pato Donald',
+// 	nota: 'Namorada do personagem principal nos quadrinhos do Pato Donald',
+//   };
+
+//   console.log('Bem-Vinda,', info.personagem)
+
+// Exercicio 2
+
+// let info = {
+// 	personagem: 'Margarida',
+// 	origem: 'Pato Donald',
+// 	nota: 'Namorada do personagem principal nos quadrinhos do Pato Donald',
+// };
+
+// info['recorrente'] = 'Sim'
+// console.log(info)
+
+// Exercicio 3
+
+// let info = {
+// 	personagem: 'Margarida',
+// 	origem: 'Pato Donald',
+// 	nota: 'Namorada do personagem principal nos quadrinhos do Pato Donald',
+// };
+
+// info['recorrente'] = 'Sim'
+// for (i in info){
+// 	console.log(i)
+// }
+
+// Exercicio 4
+
+// let info = {
+// 	personagem: 'Margarida',
+// 	origem: 'Pato Donald',
+// 	nota: 'Namorada do personagem principal nos quadrinhos do Pato Donald',
+// 	i: 'teste',
+// };
+
+// info['recorrente'] = 'Sim'
+// for (i in info){
+// 	console.log(info[i])
+// }
+// console.log(info.recorrente)
+
+// Exercicio 5
+
+// let info = {
+// 	personagem: 'Margarida',
+// 	origem: 'Pato Donald',
+// 	nota: 'Namorada do personagem principal nos quadrinhos do Pato Donald',
+// };
+
+// info['recorrente'] = 'Sim'
+
+// let info2 = {
+// 	personagem: 'tio Patinhas',
+// 	origem: "Christmas on Bear Mountain, Dell's Four Color Comics #178",
+// 	nota: 'o último MacPatinhas',
+// }
+
+// console.log(info['personagem'], 'e', info2['personagem'])
+// console.log(info['origem'], 'e', info2['origem'])
+// console.log(info['nota'], 'e', info2['nota'])
+// console.log('Ambos Recorrentes')
+
+// Exercicio 5
+
+// let leitor = {
+// 	nome: 'Julia',
+// 	sobrenome: 'Pessoa',
+// 	idade: 21,
+// 	livrosFavoritos: [
+// 	  {
+// 		titulo: 'O Pior Dia de Todos',
+// 		autor: 'Daniela Kopsch',
+// 		editora: 'Tordesilhas',
+// 	  },
+// 	],
+//   };
+
+// console.log('O livro favorito de', leitor.nome, leitor.sobrenome,'se chama', leitor.livrosFavoritos[0].titulo)
+
+// Exercicio 6
+
+// let leitor = {
+// 	nome: 'Julia',
+// 	sobrenome: 'Pessoa',
+// 	idade: 21,
+// 	livrosFavoritos: [
+// 		{
+// 			titulo: 'O Pior Dia de Todos',
+// 			autor: 'Daniela Kopsch',
+// 			editora: 'Tordesilhas',
+// 		},
+// 		{
+// 			titulo: 'Harry Potter e o Prisioneiro de Azkaban',
+// 			autor: 'JK Rowling',
+// 			editora: 'Rocco',
+// 		},
+// 	],
+// };
+// let usavel = leitor.livrosFavoritos
+// console.log('Julia tem', usavel.length, 'livros favoritos')
