f93e4a6b820375ff7dae77ff1ace4ca48d209265
Vinicius José <viniciusjosedev@gmail.com>
2022-09-27 15:07:46 -0300

    no processo de finalização

---
+++ 1. fundamentos/bloco-4-introducao-a--javascript/dia-04-javascript-objetos-e-funcoes/02-exercicios/script.js
@@ -0,0 +1,23 @@
+// Exercicio 1
+
+// function func(nome){
+// 	nomeAoContrario = ''
+// 	for (i = nome.length-1; i>=0; i-=1){
+// 		nomeAoContrario += nome[i] 
+// 	}
+// 	if (nome===nomeAoContrario){
+// 		return true
+// 	}
+// 	else{
+// 		return false
+// 	}
+// }
+// console.log(func('arara'))
+
+// Exercicio 2
+
+// Exercicio 2
+
+function func(array){
+	
+}
