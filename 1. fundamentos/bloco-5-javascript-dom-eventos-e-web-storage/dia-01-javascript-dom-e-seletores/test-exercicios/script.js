81c968af76497706e69030adc27f5eafaee0128f
Vinicius José <viniciusjosedev@gmail.com>
2022-09-29 09:33:26 -0300

    testes finalizados

--- 1. fundamentos/bloco-5-javascript-dom-eventos-e-web-storage/dia-01-javascript-dom-e-seletores/test-exercicios/script.js
+++ 1. fundamentos/bloco-5-javascript-dom-eventos-e-web-storage/dia-01-javascript-dom-e-seletores/test-exercicios/script.js
@@ -1,8 +1,12 @@
-// const paragraph = document.getElementById("paragraph");
-// paragraph.style.color = "red";
-// document.getElementById("page-title").innerText = "vai dar certo"
-// console.log('teste')
+window.document.getElementById("header-container").style.backgroundColor = 'green';
+document.querySelectorAll(".emergency-tasks")[0].style.backgroundColor = 'pink';
+document.querySelectorAll(".emergency-tasks div h3")[0].style.backgroundColor = 'purple';
+document.querySelectorAll(".emergency-tasks div h3")[1].style.backgroundColor = 'purple';
 
-// document.getElementById("second-paragraph").innerText = '2 paragrafo alterado com sucesso';
-// document.getElementById("second-paragraph").style.color = 'blue'
-// document.getElementById("subtitle").style.fontSize = '50px';
+document.getElementsByClassName("no-emergency-tasks")[0].style.backgroundColor = 'yellow';
+document.querySelectorAll(".no-emergency-tasks h3")[0].style.backgroundColor = 'black';
+
+document.querySelectorAll(".no-emergency-tasks h3")[1].style.backgroundColor = 'black';
+document.getElementsByClassName("emergency-tasks")[0].style.marginRight = '0px';
+
+document.getElementById("footer-container").style.backgroundColor = '#046400';
