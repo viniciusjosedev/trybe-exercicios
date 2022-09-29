a894f026d9ca76f0fb48b81014211aec27df4944
Vinicius José <viniciusjosedev@gmail.com>
2022-09-28 23:32:00 -0300

    testes em andamento

---
+++ 1. fundamentos/bloco-5-javascript-dom-eventos-e-web-storage/dia-01-javascript-dom-e-seletores/test-exercicios/script.js
@@ -0,0 +1,8 @@
+// const paragraph = document.getElementById("paragraph");
+// paragraph.style.color = "red";
+// document.getElementById("page-title").innerText = "vai dar certo"
+// console.log('teste')
+
+// document.getElementById("second-paragraph").innerText = '2 paragrafo alterado com sucesso';
+// document.getElementById("second-paragraph").style.color = 'blue'
+// document.getElementById("subtitle").style.fontSize = '50px';
