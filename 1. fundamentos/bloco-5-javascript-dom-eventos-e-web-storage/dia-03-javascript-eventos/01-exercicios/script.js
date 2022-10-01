de72af5c3b537f1d9d3c389b32430c8d5a16b921
Vinicius José <viniciusjosedev@gmail.com>
2022-10-01 20:09:49 -0300

    passo 1 finalizado

---
+++ 1. fundamentos/bloco-5-javascript-dom-eventos-e-web-storage/dia-03-javascript-eventos/01-exercicios/script.js
@@ -0,0 +1,83 @@
+function createDaysOfTheWeek() {
+	const weekDays = ['Domingo', 'Segunda', 'Terça', 'Quarta', 'Quinta', 'Sexta', 'Sábado']
+	const weekDaysList = document.querySelector('.week-days');
+  
+	for (let index = 0; index < weekDays.length; index += 1) {
+	  const days = weekDays[index];
+	  const dayListItem = document.createElement('li');
+	  dayListItem.innerHTML = days;
+	  weekDaysList.appendChild(dayListItem);
+	};
+  }
+  
+  createDaysOfTheWeek();
+  
+  // Escreva seu código abaixo.
+
+window.onload = days;
+
+function days() {
+	// alert ('pagina carregada com sucesso')
+	// // Criação dos ultimos dias de novembro.
+	let tagLi = '';
+	for (i = 29; i <= 30; i += 1) {
+		tagLi = document.createElement("li");
+		tagLi.className = 'day'
+		tagLi.innerText = `${i}`;
+		document.getElementById('days').appendChild(tagLi);
+	}
+	// Criação dos dias em dezembro.
+	for (i = 1; i <= 31; i += 1) {
+		tagLi = document.createElement('li');
+		tagLi.className = 'day'
+		tagLi.innerText = `${i}`
+		document.getElementById('days').appendChild(tagLi)
+	}
+	// Colocando classes nos feriados e finais de semana.
+	for (i = 0; i < document.getElementsByClassName('day').length; i += 1) {
+		if (i == parseInt('4') || i == parseInt('11') || i == parseInt('18') || i == parseInt('25')) {
+			document.getElementsByClassName('day')[i+1].className += ' friday';
+
+		}
+		if (i == parseInt('24') || i == parseInt('25') || i == parseInt('31')) {
+			document.getElementsByClassName('day')[i+1].className += ' holiday';
+
+	}
+
+
+}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  
+
+
+
+
+
+
+
+  
