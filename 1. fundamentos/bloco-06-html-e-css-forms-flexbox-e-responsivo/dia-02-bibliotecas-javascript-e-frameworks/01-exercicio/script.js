72615590388041bdec0d6b4d4b6f1b7cf0dde71f
Vinicius José <viniciusjosedev@gmail.com>
2022-10-08 07:41:55 -0300

    exercicio finalizado

---
+++ 1. fundamentos/bloco-06-html-e-css-forms-flexbox-e-responsivo/dia-02-bibliotecas-javascript-e-frameworks/01-exercicio/script.js
@@ -0,0 +1,32 @@
+window.onload = function(){
+	// document.getElementById('button').addEventListener('click', function(event) {
+	// 	event.preventDefault()
+	// })
+
+	let button = document.createElement('button')
+	button.textContent = 'Limpar'
+	button.className = "btn btn-danger"
+	button.type = 'reset'
+	document.getElementById('forms').appendChild(button)
+
+	function removeTag() {
+		document.getElementById('submitFiles').remove()
+	}
+
+	let cont = 0
+	document.getElementById('input-checkbox2').addEventListener('click', function(event) {
+		if (cont === 0) {
+			let tagInput = document.createElement('input')
+			tagInput.id = 'submitFiles'
+			tagInput.className = 'form-control form-control-sm'
+			tagInput.type = 'file'
+			document.getElementById('final').appendChild(tagInput)
+			console.log('oi')
+			cont += 1
+		} else {
+			removeTag()	
+			console.log('oi')
+			cont = 0	
+		}
+	})
+}
