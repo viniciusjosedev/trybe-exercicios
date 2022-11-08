e81eee40ca44fdc54603ed2226b664797b944b68
Vinicius José <viniciusjosedev@gmail.com>
2022-11-08 15:53:24 -0300

    fundamentos completo

---
+++ 1. fundamentos/bloco-08-Higher-Order-Functions-do-JavaScript-ES6/dia-05-JavaScript-ES6-spread-operator-parâmetro-rest-destructuring-e-mais/01-exercicio/script.js
@@ -0,0 +1,16 @@
+// Exercicio 1
+
+// const rectangleArea = (width, height) => console.log(width * height);
+
+// const rectangle1 = [1, 2];
+// const rectangle2 = [3, 5];
+// const rectangle3 = [6, 9];
+// const rectangles = [rectangle1, rectangle2, rectangle3];
+
+// rectangles.forEach((rectangle) => {
+//   return(rectangleArea(...rectangle)); // Altere o parâmetro recebido por rectangleArea()
+// });	
+
+// Exercicio 2
+
+
