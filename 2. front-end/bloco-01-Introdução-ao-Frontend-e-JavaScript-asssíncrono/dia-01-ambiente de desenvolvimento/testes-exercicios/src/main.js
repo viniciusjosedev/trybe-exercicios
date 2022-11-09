cc3fc0e9f47c04d1806de8ab2bc39f059905c900
Vinicius José <viniciusjosedev@gmail.com>
2022-11-09 11:16:32 -0300

    testes feitos

--- 2. front-end/bloco-01-Introdução-ao-Frontend-e-JavaScript-asssíncrono/dia-01-ambiente de desenvolvimento/testes-exercicios/src/main.js
+++ 2. front-end/bloco-01-Introdução-ao-Frontend-e-JavaScript-asssíncrono/dia-01-ambiente de desenvolvimento/testes-exercicios/src/main.js
@@ -3,3 +3,7 @@ import { nanoid } from 'nanoid'
 document.getElementsByTagName('button')[0].addEventListener('click', () => {
 	document.getElementsByTagName('h2')[0].innerText = nanoid();
 })
+
+function teste() {return 'a'};
+
+module.exports = teste;
