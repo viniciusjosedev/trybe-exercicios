f54a1d4d3d432bed12a37dac96338d2b26dda143
Vinicius José <viniciusjosedev@gmail.com>
2022-12-17 11:24:25 -0300

    testes feitos no 12.1

---
+++ 2. front-end/bloco-04-Ciclo-de-Vida-de-Componentes-e-React-Router/dia-01-ciclo-de-vida-de-componentes/testes-exercicios/testes-03/testes-03/src/App.jsx
@@ -0,0 +1,60 @@
+import React from 'react';
+import './App.css';
+
+class App extends React.Component {
+  constructor() {
+		super();
+		this.state = {
+			dados: [],
+		}
+	}
+
+	getDados = () => {
+		this.setState({
+			dados: [],
+		}, async () => {
+			await fetch('https://api.randomuser.me/')
+			.then(result => result.json())
+			.then(data => this.setState({
+				dados: data,
+			}))
+		})
+		// fetch('https://api.randomuser.me/')
+		// .then(result => result.json())
+		// .then(data => this.setState({
+		// 	dados: data,
+		// 	loading: true,
+		// }))
+	}
+
+	componentDidMount = () => {
+		console.log('componentDidMount')
+		this.getDados();
+	}
+
+	shouldComponentUpdate(a, b) {
+		console.log('shouldComponentUpdate')
+		if (b.dados.length === 0) return true;
+		if (b.dados.results[0].dob.age < 50) return true;
+		return false;
+	}
+
+	render() {
+		const { dados } = this.state;
+		return(
+			<>
+				<h1>Dados confidenciais</h1>
+				{dados.length !== 0 ? (
+					<>
+						<img src={ dados.results[0].picture.large } alt={`${dados.results[0].name.first} ${dados.results[0].name.last}`} />
+						<p>{ `${dados.results[0].name.first} ${dados.results[0].name.last}, ${dados.results[0].dob.age}` }</p>
+						<p>{`${dados.results[0].email}`}</p>
+					</>	
+				) :  <p>Loading...</p>}
+				<button onClick={this.getDados}>Mudar</button>
+			</>
+		)
+	}
+}
+
+export default App;
