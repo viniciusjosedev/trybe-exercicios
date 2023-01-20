a131343196290046bbca76a8be04421bc066dd21
Vinicius José <viniciusjosedev@gmail.com>
2023-01-19 22:30:31 -0300

    testes feitos

--- 2. front-end/bloco-06-testes-automatizados-com-RTL/dia-03-testando-react-router/tests/testes/src/renderWithRouter.js
+++ 2. front-end/bloco-06-testes-automatizados-com-RTL/dia-03-testando-react-router/tests/testes/src/renderWithRouter.js
@@ -1,13 +1,13 @@
 // src/renderWithRouter.js
 import React from 'react';
-import { Router } from 'react-router-dom';
+import { Route } from 'react-router-dom';
 import { createMemoryHistory } from 'history';
 import { render } from '@testing-library/react';
 
 const renderWithRouter = (component) => {	
   const history = createMemoryHistory();
   return ({
-    ...render(<Router history={ history }>{component}</Router>), history,
+    ...render(<Route history={ history }>{component}</Route>), history,
   });
 };
 export default renderWithRouter;
