4ea0bfe8f798d3a753806496f73758f8dc2ed46e
Vinicius José <viniciusjosedev@gmail.com>
2023-03-27 15:09:45 -0300

    more updates

---
+++ 3. back-end/bloco-02-mysql/dia-03-filtrando-dados-de-forma-especifica/exercicios/exercicio-01.sql
@@ -0,0 +1,15 @@
+USE store;
+-- Vamos lá! Faça uma consulta na tabela item que retorne todos os dados em que a coluna name comece com as letras GR. 
+SELECT * FROM itens WHERE name LIKE 'GR%';
+-- Agora, escreva uma query para retornar da tabela supply os dados em que a coluna item_id possua o valor 2. Organize o resultado por ordem alfabética de supplier_id.
+SELECT * FROM supplies WHERE item_id = 2 ORDER BY supplier_id;
+-- Em seguida, faça uma consulta para exibir item_id, price e supplier_id de supply em que o valor de supplier_id tenha a letra N.
+SELECT item_id, price, supplier_id FROM supplies WHERE supplier_id LIKE '%N%';
+-- Avante, falta pouco! Escreva uma query para exibir todas as informações de supplier que são empresas limitadas (LTDA). Ordene esses resultados em ordem alfabética decrescente.
+SELECT * FROM suppliers WHERE name LIKE '%LTDA%' ORDER BY name DESC;
+-- Agora, faça uma consulta para exibir o número de empresas (supplier) que contém a letra F no código.
+  SELECT COUNT(id) AS Quantidade FROM suppliers WHERE id LIKE '%F%';
+-- Quase lá! Agora escreva uma query para exibir de supply, itens que custam mais de R$15,00 e menos de $40,00. Ordene os resultados por ordem crescente.
+ SELECT * FROM supplies WHERE price >= 15 AND price <= 40 ORDER BY price;
+-- Ufa! Por fim, faça uma query para exibir o número de vendas (sale) feitas entre os dias 15/04/2018 e 30/07/2019.
+SELECT * FROM sales WHERE DATE(order_date) BETWEEN '2018-04-15' AND '2019-07-30';
---
+++ 3. back-end/bloco-02-mysql/dia-03-filtrando-dados-de-forma-especifica/tests/teste-02.sql
@@ -0,0 +1,67 @@
+-- Exclui o banco de dados caso já exista
+DROP SCHEMA IF EXISTS PiecesProviders;
+
+-- Cria o banco PiecesProviders
+CREATE SCHEMA PiecesProviders;
+USE PiecesProviders;
+
+-- Cria a tabela Pieces
+CREATE TABLE Pieces (
+  Code INTEGER PRIMARY KEY NOT NULL,
+  Name TEXT NOT NULL
+);
+
+-- Cria a tabela Providers
+CREATE TABLE Providers (
+  Code VARCHAR(40) PRIMARY KEY NOT NULL,
+  Name TEXT NOT NULL
+);
+
+-- Cria a tabela Provides
+CREATE TABLE Provides (
+  Piece INTEGER,
+  FOREIGN KEY (Piece) REFERENCES Pieces (Code),
+  Provider VARCHAR(40),
+  FOREIGN KEY (Provider) REFERENCES Providers (Code),
+  Price INTEGER NOT NULL,
+  PRIMARY KEY (Piece , Provider)
+);
+
+-- Insere valores na tabela Providers
+INSERT INTO Providers(Code, Name)
+  VALUES ('HAL', 'Clarke Enterprises'),
+    ('RBT', 'Susan Calvin Corp.'),
+    ('TNBC', 'Skellington Supplies');
+
+-- Insere valores na tabela Pieces
+INSERT INTO Pieces(Code, Name)
+  VALUES (1, 'Sprocket'),
+    (2, 'Screw'),
+    (3, 'Nut'),
+    (4, 'Bolt');
+
+-- Insere valores na tabela Provides
+INSERT INTO Provides(Piece, Provider, Price)
+  VALUES (1, 'HAL', 10),
+    (1, 'RBT', 15),
+    (2, 'HAL', 20),
+    (2, 'RBT', 25),
+    (2, 'TNBC', 14),
+    (3, 'RBT', 50),
+    (3, 'TNBC', 45),
+    (4, 'HAL', 5),
+    (4, 'RBT', 7);
+    
+-- Escreva uma query para exibir a peça e o preço de tudo que é provido pela empresa RBT.
+SELECT Piece, Price FROM PiecesProviders.Provides 
+WHERE Provider = 'RBT';
+-- Escreva uma query para exibir todas as informações das cinco peças com os maiores preços.
+SELECT * FROM PiecesProviders.Provides ORDER BY Price DESC LIMIT 5;
+-- Escreva uma query para exibir o nome das empresas e o preço das quatro peças com os maiores preços, começando a lista a partir do 3º item.
+SELECT Provider, Price FROM PiecesProviders.Provides ORDER BY Price DESC LIMIT 4 OFFSET 3;
+-- Escreva uma query para exibir todas as informações das peças que são providas pela empresa HAL. Ordene o resultado pelos preços das peças de forma decrescente.
+SELECT * FROM PiecesProviders.Provides WHERE Provider = 'HAL' ORDER BY Price DESC;
+-- Escreva uma query para exibir por quantas empresas a peça 1 é provida.
+SELECT CONCAT('A peça 1 é provida por ', Empresas, ' empresas') AS Informações FROM (SELECT COUNT(Piece) AS Empresas FROM PiecesProviders.Provides WHERE Piece = 1 OR Piece = 2)Empresas;
+
+
